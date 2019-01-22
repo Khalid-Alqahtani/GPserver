cb97768b0e93e49c37a25f427fa2ffae0040da10
Khalid <434105057@student.ksu.edu.sa>
2019-01-22 20:07:08 +0300

    Init

---
+++ app.js
@@ -0,0 +1,14 @@
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({port: 1000});
+
+wss.on('connection', function connection(ws) {
+
+    ws.on('message', function incoming(message) {
+        console.log(message);
+        ws.send(message);
+    });
+
+
+
+});
